755297c08f6ce3fa5703d2810034156c4028e92d
Park Jin <tstunas@gmail.com>
2025-09-21 19:01:02 +0900

    feat(web-client): configure app config

--- apps/aa-community-web-client/src/app/app.config.ts
+++ apps/aa-community-web-client/src/app/app.config.ts
@@ -1,13 +1,37 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withInMemoryScrolling,
+  withRouterConfig,
+  withViewTransitions,
+} from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { COMPOSITION_BUFFER_MODE } from '@angular/forms';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay()),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(appRoutes),
+    provideRouter(
+      appRoutes,
+      withComponentInputBinding(),
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled',
+      }),
+      withViewTransitions(),
+      withRouterConfig({
+        paramsInheritanceStrategy: 'always',
+      }),
+    ),
+    provideHttpClient(withFetch(), withInterceptors([])),
+    {
+      provide: COMPOSITION_BUFFER_MODE,
+      useValue: false,
+    },
   ],
 };
