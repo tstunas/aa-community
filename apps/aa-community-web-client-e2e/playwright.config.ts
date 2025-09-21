be3b7d6722ee54982246a078b3771303fe1c979f
Park Jin <tstunas@gmail.com>
2025-09-21 18:24:51 +0900

    chore(e2e): add mobile browser supprots

--- apps/aa-community-web-client-e2e/playwright.config.ts
+++ apps/aa-community-web-client-e2e/playwright.config.ts
@@ -46,14 +46,14 @@ export default defineConfig({
     },
 
     // Uncomment for mobile browsers support
-    /* {
+    {
       name: 'Mobile Chrome',
       use: { ...devices['Pixel 5'] },
     },
     {
       name: 'Mobile Safari',
       use: { ...devices['iPhone 12'] },
-    }, */
+    },
 
     // Uncomment for branded browsers
     /* {
