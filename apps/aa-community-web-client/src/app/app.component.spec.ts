64d5b8edf109332db525eb8ecdd147db129ef349
Park Jin <tstunas@gmail.com>
2025-09-21 18:14:27 +0900

    style: prettier files

--- apps/aa-community-web-client/src/app/app.component.spec.ts
+++ apps/aa-community-web-client/src/app/app.component.spec.ts
@@ -13,8 +13,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome aa-community-web-client'
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome aa-community-web-client');
   });
 });
--- apps/aa-community-web-client/src/app/app.config.ts
+++ apps/aa-community-web-client/src/app/app.config.ts
@@ -1,14 +1,7 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
