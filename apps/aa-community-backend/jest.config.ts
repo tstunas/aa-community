b7bbc6af30021266c6fd32b3ec28bac7414b17d0
Park Jin <tstunas@gmail.com>
2025-09-21 17:57:44 +0900

    feat(apps): add backend nestjs app

---
+++ apps/aa-community-backend/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'aa-community-backend',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/aa-community-backend',
+};
